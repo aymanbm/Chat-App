5e410fa4c97a16137da0af75cdb22dfc651a7750
AYMANE EL MAGHRAOUI <ayman.elmaghraoui.5@gmail.com>
2024-09-02 12:58:06 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Bind to all network interfaces
+  css: {
+    postcss: './postcss.config.js',
   },
+  // server: {
+  //   host: '0.0.0.0',  // Bind to all network interfaces
+  // },
 })
